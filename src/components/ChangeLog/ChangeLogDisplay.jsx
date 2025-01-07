8db44798faabbad8419c07e4a70e8b2081d02673
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-01-07 13:20:40 -0800

    Added responsiveness to the height class in bootstrap and now the various columns in the main container utilize bootstrap to change their height appropriately based on the current breakpoint

--- src/components/ChangeLog/ChangeLogDisplay.jsx
+++ src/components/ChangeLog/ChangeLogDisplay.jsx
@@ -17,10 +17,10 @@ export function ChangeLogDisplay({ endPoint, changeLog, alterChangeLog }) {
 
 
     return (
-        <div>
+        <>
             {undoActionError && <h3>{undoActionError}</h3>}
             {changeLog.length == 0 && <h2>No Changes To Display</h2>}
             {changeLog && changeLog.map(change => <ChangeInfo key={`changeLog-item-${change.id}`} info={change} undoChange={undoChange} isButtonDisabled={isUndoingAction} />)}
-        </div>
+        </>
     )
 } 
--- src/components/InvoiceOrganizerUI/MainContainer.jsx
+++ src/components/InvoiceOrganizerUI/MainContainer.jsx
@@ -15,12 +15,12 @@ export function MainContainer(props) {
 
     return (
         <Row className={`${styles.mainContainer}`}>
-            <Col mobilePortrait={12} mobileLandscape={6} tabletPortrait={12} tabletLandscape={4} className={`p-0`}>
+            <Col mobilePortrait={12} mobileLandscape={6} tabletPortrait={12} tabletLandscape={4} className={`p-0 h-25 h-mobileLandscape-100 h-tabletPortrait-25 h-tabletLandscape-100`}>
                 <Container>
                     <Row>
                         <Col mobilePortrait={12} tabletPortrait={9} tabletLandscape={12} className="p-0 order-1 order-tabletPortrait-2 order-tabletLandscape-1">
                             <Stack gap={2}>
-                                <Stack direction="horizontal" gap={1} >
+                                <Stack direction="horizontal" gap={1}>
                                     <DirectoryFilter styles={styles} filter={[props.directoryFilter, props.alterDirectoryFilter]} isDisabled={props.isUserInteractionDisabled} />
                                     <YearSelector styles={styles} isDisabled={props.isUserInteractionDisabled} />
                                 </Stack>
@@ -31,14 +31,16 @@ export function MainContainer(props) {
                         </Col>
                     
                         {/* Add the user setting to control how many changeLog actions are displayed in the quick view*/}
-                        <Col className="d-none d-tabletPortrait-flex order-2 order-tabletPortrait-1 order-tabletLandscape-2 p-0" mobilePortrait={12} tabletPortrait={3} tabletLandscape={12}>
-                            <ChangeLogDisplay endPoint={`${props.endPointURL}/${props.pageName}`} changeLog={props.changeLog.slice(0)} alterChangeLog={props.alterChangeLog} />
+                        <Col className={`d-none d-tabletPortrait-flex order-2 order-tabletPortrait-1 order-tabletLandscape-2 p-0`} mobilePortrait={12} tabletPortrait={3} tabletLandscape={12}>
+                            <Stack className={`${styles.changeLogDisplay}`}>
+                                <ChangeLogDisplay endPoint={`${props.endPointURL}/${props.pageName}`} changeLog={props.changeLog.slice(0)} alterChangeLog={props.alterChangeLog} />
+                            </Stack>
                         </Col>
                     </Row>
                 </Container>
             </Col>
 
-            <Col mobilePortrait={12} mobileLandscape={6} tabletPortrait={12} tabletLandscape={8} className={`p-0`}>
+            <Col mobilePortrait={12} mobileLandscape={6} tabletPortrait={12} tabletLandscape={8} className={`p-0 h-75 h-mobileLandscape-100 h-tabletPortrait-75 h-tabletLandscape-100`}>
                 <InvoiceViewer updateIsLoadingBoolean={props.updateIsLoadingBoolean} endPoint={`${props.endPointURL}/${props.pageName}`} fetchKey={`${props.pageName}-invoiceViewer`} />
             </Col>
         </Row>
