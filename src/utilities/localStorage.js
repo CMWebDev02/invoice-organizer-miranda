32db090c27433e649936f57fe4d8d1bca64dfebc
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-01-20 10:38:50 -0800

    Finished adding comments to the various utility files and reformated the documents using Prettier extension

--- src/utilities/localStorage.js
+++ src/utilities/localStorage.js
@@ -1,62 +1,113 @@
+/**
+ * @class Default class for accessing localStorage.
+ */
 class accessLocalStorage {
-    static resetStorage() {
-        let userConfirmation = confirm('Warning: This action is irreversible! Are you sure you want to continue?')
-        if (!userConfirmation) return;
+  /**
+   * @method Removes the localStorage entry using the key associated with the class.
+   * @returns {void}
+   */
+  static resetStorage() {
+    let userConfirmation = confirm(
+      "Warning: This action is irreversible! Are you sure you want to continue?"
+    );
+    if (!userConfirmation) return;
 
-        localStorage.removeItem(this._key);
-    }
+    localStorage.removeItem(this._key);
+  }
 }
 
+/**
+ * @class Default class for accessing the changeLog entries in localStorage.
+ */
 export class ChangeLogStorage extends accessLocalStorage {
-    static setStorage(array, maxActionStorage) {
-        if (maxActionStorage > array.length) {
-            localStorage.setItem(this._key, JSON.stringify(array));
-        } else {
-            let newArray = array.slice(0, maxActionStorage);
+  /**
+   * @method Sets the localStorage entry for the associated class using the passed in array, and depending on the size of the array and the user's storage preferences, the array
+   * will be trimmed down to not exceed their maximum storage allowance.
+   * @param {Array} array - Array to be stored in localStorage.
+   * @param {number} maxActionStorage - The maximum amount of entries allowed within the localStorage entry.
+   * @returns {void}
+   */
+  static setStorage(array, maxActionStorage) {
+    if (maxActionStorage > array.length) {
+      localStorage.setItem(this._key, JSON.stringify(array));
+    } else {
+      let newArray = array.slice(0, maxActionStorage);
 
-            localStorage.setItem(this._key, JSON.stringify(newArray))
-        }
+      localStorage.setItem(this._key, JSON.stringify(newArray));
     }
+  }
 
-    static getStorage() {
-        return JSON.parse(localStorage.getItem(this._key)) || [];
-    }
+  /**
+   * @method Returned the localStorage entry for the associated key.
+   * @returns {array}
+   */
+  static getStorage() {
+    return JSON.parse(localStorage.getItem(this._key)) || [];
+  }
 }
 
+/**
+ * @class Class associated with the customer-scanned-documents changelog.
+ */
 export class CustomerScannedDocumentsChangeLog extends ChangeLogStorage {
-    static _key = 'customerScannedDocs';
+  //* Initializes the localStorage key as a private class attribute.
+  static _key = "customerScannedDocs";
 }
 
+/**
+ * @class Class associated with the accounts-payables changelog.
+ */
 export class AccountsPayablesChangeLog extends ChangeLogStorage {
-    static _key = 'accountsPayables';
+  //* Initializes the localStorage key as a private class attribute.
+  static _key = "accountsPayables";
 }
 
+/**
+ * @class Class associated with storing the user's settings in localStorage
+ */
 export class UserSettingsStorage extends accessLocalStorage {
-    static _key = 'userSettings';
+  //* Initializes the localStorage key as a private class attribute.
+  static _key = "userSettings";
 
-    static _defaultSettingsObj = {
-        CHANGELOG_ACTIONS: 25,
-        CHANGELOG_QUICK_VIEW: 15,
-        YEAR_OFFSET: 5,
-        SHOW_QUICK_TRANSFER_BUTTONS: true,
-        SHOW_QUICK_UNDO_BUTTONS: true,
-    }
+  //* Initializes a default settings object.
+  static _defaultSettingsObj = {
+    CHANGELOG_ACTIONS: 25,
+    CHANGELOG_QUICK_VIEW: 15,
+    YEAR_OFFSET: 5,
+    SHOW_QUICK_TRANSFER_BUTTONS: true,
+    SHOW_QUICK_UNDO_BUTTONS: true,
+  };
 
-    static setStorage(obj) {
-        localStorage.setItem(this._key, JSON.stringify(obj));
-    }
+  /**
+   * @method Sets the localStorage entry for the user's settings
+   * @param {object} obj - Settings object to be stored in localStorage.
+   * @returns {void}
+   */
+  static setStorage(obj) {
+    localStorage.setItem(this._key, JSON.stringify(obj));
+  }
 
-    static getStorage() {
-        // Checks if localStorage has a value associated with the userSettings key and if not, then the default settings object is stored in localStorage.
-        if (localStorage.getItem(this._key) == null) this.setStorage(this._defaultSettingsObj)
+  /**
+   * @method Retrieves the settings object stored in localStorage or returns the defaultSettingsObject if localStorage returns a null value.
+   * @returns {object}
+   */
+  static getStorage() {
+    // Checks if localStorage has a value associated with the userSettings key and if not, then the default settings object is stored in localStorage.
+    if (localStorage.getItem(this._key) === null)
+      this.setStorage(this._defaultSettingsObj);
 
-        return JSON.parse(localStorage.getItem(this._key));
+    return JSON.parse(localStorage.getItem(this._key));
+  }
 
-    }
+  /**
+   * @method Returns the values of a specific setting stored in localStorage.
+   * @param {string} setting - Key name that will be used to pull its associated value from the settings object.
+   * @returns {number | boolean}
+   */
+  static getSpecificSetting(setting) {
+    let allSettings = this.getStorage();
 
-    static getSpecificSetting(setting) {
-        let allSettings = this.getStorage();
-
-        return allSettings[setting];
-    }
+    // After retrieving all settings from localStorage, the passed in string is used to define the key.
+    return allSettings[setting];
+  }
 }
--- src/utilities/stringMutations.js
+++ src/utilities/stringMutations.js
@@ -1,32 +1,60 @@
+/**
+ * @function Converts first character of the passed in string to a capital.
+ * @returns {string}
+ */
 function capitalizeString(string) {
-    return string[0].toUpperCase() + string.substring(1, string.length)
+  return string[0].toUpperCase() + string.substring(1, string.length);
 }
 
+/**
+ * @function Converts the passed in string to a valid query string.
+ * @returns {string}
+ */
 export function convertToValidQueryString(name) {
-    let regexPattern = /\W+/g;
-    return name.replace(regexPattern, '%20')
+  let regexPattern = /\W+/g;
+  return name.replace(regexPattern, "%20");
 }
 
+/**
+ * @function Appends query parameters to the end of a URL.
+ * @param {string} baseURL - Main URL that the queries will be appended to.
+ * @param {object} baseURL - An object of the various queries that will be appended to the end of the URL, parameter name is the key and parameter value is the value.
+ * @returns {string}
+ */
 export function appendQueriesParameters(baseURL, queries) {
-    let fetchURL = `${baseURL}?`;
-    for (const [key, value] of Object.entries(queries)) {
-        fetchURL += `${key}=${value}&`;
-    };
-    return fetchURL.substring(0, fetchURL.length - 1);
+  let fetchURL = `${baseURL}?`;
+  for (const [key, value] of Object.entries(queries)) {
+    fetchURL += `${key}=${value}&`;
+  }
+  return fetchURL.substring(0, fetchURL.length - 1);
 }
 
+/**
+ * @function Converts the passed in string to a title by replacing all '_' with spaces and capitalizing each word.
+ * @param {string}
+ * @returns {string}
+ */
 export function convertToTitle(originalString) {
-    let convertedString = originalString.toLowerCase();
-    let convertedStringArray = convertedString.split('_');
-    return convertedStringArray.map(word => {
-        return capitalizeString(word)
-    }).join(' ')
+  let convertedString = originalString.toLowerCase();
+  let convertedStringArray = convertedString.split("_");
+  return convertedStringArray
+    .map((word) => {
+      return capitalizeString(word);
+    })
+    .join(" ");
 }
 
+/**
+ * @function Converts a string written in spinal-tap to a title string where each word is capitalized.
+ * @param {type} variable - description.
+ * @returns
+ */
 export function convertFromSpinalTap(originalString) {
-    let convertedString = originalString.toLowerCase();
-    let convertedStringArray = convertedString.split('-');
-    return convertedStringArray.map(word => {
-        return capitalizeString(word)
-    }).join(' ')
+  let convertedString = originalString.toLowerCase();
+  let convertedStringArray = convertedString.split("-");
+  return convertedStringArray
+    .map((word) => {
+      return capitalizeString(word);
+    })
+    .join(" ");
 }
