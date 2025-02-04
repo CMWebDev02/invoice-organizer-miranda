34a11439695cf00c7b388dde7a11c826188486dd
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-02-03 19:08:24 -0800

    Removed unneeded import

--- src/utilities/SessionValidator.jsx
+++ src/utilities/SessionValidator.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useLocation, useNavigate } from "react-router";
 
